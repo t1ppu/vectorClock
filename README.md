1089a14845abef038ea99ec14112a90a674ea2c9
t1ppu <kollisriharrsha@gmail.com>
2023-09-10 18:09:53 -0500

    Files

---
+++ README.md
@@ -0,0 +1,21 @@
+
+#  Vector Clock implementation
+
+Using logical clock to timestamp messages sent/recieved among the nodes.
+
+## Requirements
+Python 3.0+ with the following libraries installed-
+
+- sockets
+- threads
+
+## Running the Program
+
+1. Open a terminal in the all the machines.
+2. Execute the program.
+   $python test1.py
+3. The machine starts listening for incoming messages.
+4. If the user wants to send a message, he can press any key and a prompt will be displayed.
+5. The recieved messages are shown in the terminal along with the vector clocks.
+
+
---
+++ test2.py
@@ -0,0 +1,91 @@
+import socket
+import numpy as np
+from threading import Thread
+import time
+import logging
+import traceback
+
+ip= "127.0.0.1"
+port1= 8002
+port2= 8005
+sock= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+i=1
+v = [0,0,0]
+
+def send_msg():
+    global v
+    print("-----Press any key to send a message----")
+    while True:
+        x=input()
+        if x:
+            sock2=  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock2.bind((ip,port2))
+            while True:
+                no=input("Enter machine number (1 or 3): ")
+                if no not in ('1','3'):
+                    print("Please enter correct number.")
+                else:
+                    fport= 8000+int(no)
+                    v[i]+=1
+                    msg=''.join(str(x)+' ' for x in v) + ' '
+                    inp=input("Enter the message to send:")
+                    print(v,end='')
+                    msg+=inp
+                    try:
+                        try:
+                            sock2.sendto(msg.encode(),(ip,fport))
+                        except:
+                            sock2=  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                            sock2.connect((ip,fport))
+                            sock2.sendto(msg.encode(),(ip,fport))
+                            #sock2.close()
+                    except Exception as e:
+                        logging.error(traceback.format_exc())
+                        print("Couldn't send msg!")
+                        v[i]-=1
+                    print(v)
+                    break
+            sock2.shutdown(socket.SHUT_RDWR)
+        time.sleep(3)
+
+def rec_msg(sock,addr):
+    global v
+    #print('Got a msg from', addr)
+    try:
+        msg = sock.recv(1024)
+        print(v,end='')
+        v[i]+=1
+        #print(msg.decode().split('  ')[1])
+        l= [int(x) for x in list(msg.decode().split('  ')[0]) if x.isdigit()]
+        v=np.maximum(v,l)
+        print(v,end='')
+        print(msg.decode().split('  ')[1])
+        #print(msg.decode())
+    except Exception as e:
+        logging.error(traceback.format_exc())
+
+if __name__ == "__main__":
+    print("Waiting for msgs")
+    sock.bind((ip, port1))
+    sock.listen(3)
+    #sock2.bind((ip,port2))
+    print(sock)
+    t1=None
+    t2=None
+    t1= Thread(target=send_msg,args=(),daemon=True)
+    t1.start()
+    while True:
+        try:
+            conn, addr= sock.accept()
+            #print(conn)
+            if conn:
+                t2= Thread(target=rec_msg,args=(conn,addr,), daemon=True)
+                t2.start()
+            time.sleep(1)
+        except Exception as e:
+            logging.error(traceback.format_exc())
+    # print("xx")
+    # t1.join()
+    # t2.join()
+    # #s.close()
